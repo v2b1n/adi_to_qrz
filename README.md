430f10dfe874b0fb85d832db5cae63de79781d80
Vladimir Vecgailis <vladimir@vovka.de>
2019-02-10 18:38:15 +0100

    Initial release: v0.1

--- README.md
+++ README.md
@@ -1,3 +1,32 @@
-# adi_to_qrz
+## adi_to_qrz
 
-This python script uploads your ADI-logfile to qrz.com logbook
+This python script uploads your ADI-logfile to qrz.com logbook.
+
+## Why?
+My WSJT-X runs on a raspberry pi. After having used the WSJT-X for a while i noticed that the manual upload-procedure to qrz.com is rather boring.
+This script should fix it - being triggered every 10 minutes by cron, it grabs the "wsjtx_log.adi" and the uploads the QSO-records.
+
+
+## Usage
+
+As the "help" sections shows, the usage is pretty straightforward
+
+```
+./adi_to_qrz.py  -h
+Usage: adi_to_qrz.py [options]
+ -h  --help      print this usage and exit
+ -a  --apikey    setting apikey for api-connection
+ -i  --inputfile setting inputfile, default: wsjtx_log.adi
+ -l  --logfile   setting logfile, default: adi_to_qrz.log
+ -d  --delete    empty the inputfile after import, default: no
+```
+
+The only mandatory option is "-a" - sure, you have to provide a valid API-key for QRZ.com logbook-access. Where do you get one?
+Please, just google for it https://www.google.de/search?q=qrz+logbook+api+key.
+
+Beside of specifying the apikey as "-a" option you can also set it as environment variable "APIKEY".
+
+All actions are logged into a logfile.
+To disable logfile writing entirely specify "-l null".
+
+All ADI-log-records rejected by QRZ-server are stored into a file that is named "YYYMMDD_HHmmfailed_records.adi", where YYYYMMDD_HHmm is the current date and time.
---
+++ adi_to_qrz.py
@@ -0,0 +1,157 @@
+#!/usr/bin/env python
+
+# Author: Vladimir Vecgailis <vladimir@vovka.de>, DO2VV
+# https://www.vovka.de/v2b1n/adi_to_qrz/
+# 
+# This program is distributed under terms of GPL.
+#
+# v0.1
+
+import io
+import logging
+import os
+import requests
+import sys
+import time
+import datetime
+import getopt
+
+import logging
+logging.getLogger("requests").setLevel(logging.WARNING)
+logging.getLogger("urllib3").setLevel(logging.WARNING)
+
+logger = logging.getLogger()
+formatter = logging.Formatter('%(asctime)s %(levelname)-8s %(message)s')
+handler = logging.StreamHandler()
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+logger.setLevel(logging.DEBUG)
+PATH = os.path.dirname(os.path.abspath(__file__))
+
+apikey = "APIKEY_NOT_SPECIFIED"
+apiurl = "https://logbook.qrz.com/api"
+logfile = os.path.basename(__file__).split(".")[0] + ".log"
+inputfile = "wsjtx_log.adi"
+failed_records = []
+delete = False
+
+def add_record(record):
+    global apikey
+    global apiurl
+    payload = {'KEY': apikey, 'ACTION': 'INSERT', 'ADIF': record }
+    try:
+        r = requests.post(apiurl, data = payload)
+        if ( r.status_code == 200 ):
+            params = dict(x.split('=') for x in r.text.split('&'))
+
+            id = (record.split(" ")[0]).split(">")[1]
+            if 'RESULT' in params:
+                if params['RESULT'] == "OK":
+                    logger.info("QSO record with "+ id +" added")
+                else:
+                    failed_records.append(record)
+                    if 'REASON' in params:
+                        reason = params['REASON']
+                    else:
+                        reason = "No failure reasons provided by server"
+                    logger.error("Insert of QSO with " + id +" failed(Server response was:\"" + reason +"\")")
+                    logger.error("Failed record: " + record )
+
+            if 'STATUS' in params:
+                if params['STATUS'] == "FAIL":
+                    failed_records.append(record)
+                    if 'REASON' in params:
+                        reason = params['REASON']
+                    else:
+                        reason = "No failure reasons provided by server"
+
+                    logger.error("Insert of QSO with " + id +" failed(Server response was:\"" + reason +"\")")
+                    logger.error("Failed record: " + record )
+            
+    except:
+        logger.error("Could not connect to "+ url)
+        exit(1)
+
+def print_help():
+    print "Usage: " + os.path.basename(__file__) + " [options]"
+    print " -h  --help      print this usage and exit"
+    print " -a  --apikey    setting apikey for api-connection"
+    print " -i  --inputfile setting inputfile, default: wsjtx_log.adi"
+    print " -l  --logfile   setting logfile, default: "+ os.path.basename(__file__).split(".")[0] + ".log"
+    print " -d  --delete    empty the inputfile after import, default: no"
+    exit(0)
+
+def main():
+    global logfile
+    global apikey
+    global inputfile
+    global delete
+
+    if 'APIKEY' in os.environ:
+        apikey = os.environ['APIKEY']
+
+    # grab opts
+    options, remainder = getopt.gnu_getopt(sys.argv[1:],
+        'l:a:hdi:',
+        ['logfile=', 'apikey=', 'help', 'delete', 'inputfile=', ])
+
+    # check opts
+    for opt, arg in options:
+	if opt in ('-l', '--logfile'):
+	    logfile = arg
+	elif opt in ('-a', '--apikey'):
+	    apikey = arg
+	elif opt in ('-d', '--delete'):
+	    delete = True
+	elif opt in ('-h', '--help'):
+	    print_help()
+	elif opt in ('-i', '--inputfile'):
+	    inputfile = arg
+
+    # now check whether everything needed is given - at least apikey & inputfile
+    # must be present
+    if apikey in ('', 'APIKEY_NOT_SPECIFIED'):
+        logger.error("API key for qrz.com not specified. Please use either \"-a\" key or set environment variable \"APIKEY\".")
+        exit(2)
+
+    if not os.path.isfile(inputfile):
+        logger.error("The inputfile " + inputfile + " does not exist")
+        exit(3)
+
+    # AND write a file
+    if logfile != "null":
+        logging.getLogger().addHandler(logging.FileHandler(logfile))
+    
+    with open(inputfile) as f:
+        lines = [line.rstrip('\n') for line in open(inputfile)]
+
+    if len(lines) < 1:
+        logger.error("The source file " + inputfile + " is empty")
+        exit(1)
+
+    # per record - add 
+    for line in lines:
+        if line.startswith("<call") or line.startswith("<CALL"):
+            add_record(line)
+
+    # now, if there are any failed records - write them into a separate file
+    if len(failed_records) > 0:
+        failed_records_file = datetime.datetime.now().strftime("%Y%m%d_%H%M%S") + "_failed_records.adi"
+        logger.warn("Writing " + str(len(failed_records)) + " failed records into file " + failed_records_file)
+        f = open(failed_records_file, "w")
+        for failed in failed_records:
+            f.write(failed + "\n")
+        f.close();
+
+    # now empty the source file
+    if delete == True:
+        logger.info("Emptying the source file " + inputfile)
+        f = open(inputfile, "w")
+        f.close()
+
+    
+
+########################################
+
+if __name__ == "__main__":
+    main()
